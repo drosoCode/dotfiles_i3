cae2ad9650bf1c300bf13dec1852c5d23aa2ee90
drosoCode <contact@thomasz.me>
2022-07-07 17:57:56 +0000

    update dotfiles

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+abbr -g dc 'docker-compose'
+abbr -g cpr 'rsync -rh --progress'
